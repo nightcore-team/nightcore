3ac9e6f20bd567761b0c9ea4c83ee9749e73da88
monok8i <didkivskiyyura725@gmail.com>
2025-08-21 23:00:04 +0200

    Replace dynamic GuildLoggingConfig references with string keys in LoggingChannelType enum

--- src/infra/db/models/_enums.py
+++ src/infra/db/models/_enums.py
@@ -2,14 +2,19 @@
 
 from enum import Enum
 
-from src.infra.db.models.guild import (
-    GuildLoggingConfig,
-)
-
 
 class LoggingChannelType(Enum):
     """Enumeration for logging channel types."""
 
-    REACTIONS = GuildLoggingConfig.reactions_log_channel_id
-    MESSAGES = GuildLoggingConfig.messages_log_channel_id
-    IGNORE = GuildLoggingConfig.message_log_ignoring_channels_ids
+    BANS = "bans_log_channel_id"
+    CLANS = "clans_log_channel_id"
+    MEMBERS = "members_log_channel_id"
+    MESSAGES = "messages_log_channel_id"
+    VOICES = "voices_log_channel_id"
+    MODERATION = "moderation_log_channel_id"
+    TICKETS = "tickets_log_channel_id"
+    ROLES = "roles_log_channel_id"
+    CHANNELS = "channels_log_channel_id"
+    REACTIONS = "reactions_log_channel_id"
+    PRIVATE_CHANNELS = "private_rooms_log_channel_id"
+    IGNORE = "message_log_ignoring_channels_ids"
