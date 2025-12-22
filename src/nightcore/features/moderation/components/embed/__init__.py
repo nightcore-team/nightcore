9530c9fe4750568c46047caa25428416404c89d7
monok8i <didkivskiyyura725@gmail.com>
2025-12-22 17:26:17 +0100

    refactor(moderation): remove unused DM embed functions from exports

--- src/nightcore/features/moderation/components/embed/__init__.py
+++ src/nightcore/features/moderation/components/embed/__init__.py
@@ -1,11 +1,5 @@
 from .punish import (
-    generate_dm_punish_embed,
-    generate_dm_un_punish_embed,
     generate_log_punish_embed,
 )
 
-__all__ = (
-    "generate_dm_punish_embed",
-    "generate_dm_un_punish_embed",
-    "generate_log_punish_embed",
-)
+__all__ = ("generate_log_punish_embed",)
