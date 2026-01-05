fb52c597a24719b6b255aa53048147aba266121b
max <merzlyakov029@gmail.com>
2026-01-05 07:09:04 +0500

    refactor(faq): use walrus operator for safer image_url handling

--- src/nightcore/features/faq/components/v2/view/faq.py
+++ src/nightcore/features/faq/components/v2/view/faq.py
@@ -201,8 +201,8 @@ class FAQPageViewV2(LayoutView):
         container.add_item(TextDisplay[Self](f"{page['content']}"))
         container.add_item(Separator[Self]())
 
-        if page["image_url"] is not None:
-            media_gallery = MediaGalleryItem(page["image_url"])
+        if (image_url := page.get("image_url", None)) is not None:
+            media_gallery = MediaGalleryItem(image_url)
             container.add_item(MediaGallery[Self](media_gallery))
 
         now = datetime.now(UTC)
