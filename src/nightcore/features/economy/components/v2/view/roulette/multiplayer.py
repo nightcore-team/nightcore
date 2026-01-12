624fbe2354f77127f20c8fedd2b4f9af4dc48cec
monok8i <didkivskiyyura725@gmail.com>
2026-01-12 17:37:28 +0100

    feat(casino): update multiplayer roulette view with game state and results

--- src/nightcore/features/economy/components/v2/view/roulette/multiplayer.py
+++ src/nightcore/features/economy/components/v2/view/roulette/multiplayer.py
@@ -23,6 +23,10 @@ if TYPE_CHECKING:
     from src.infra.db.models._annot import CasinoBetAnnot
     from src.nightcore.bot import Nightcore
 
+from src.infra.db.models._enums import (
+    CasinoBetResultTypeEnum,
+    CasinoGameStateEnum,
+)
 from src.nightcore.features.economy.utils.casino import COLORS
 
 
@@ -34,6 +38,8 @@ class MultiplayerRouletteViewV2(LayoutView):
         initiator_id: int,
         initiator_bet: int,
         initiator_selected_color: str,
+        state: CasinoGameStateEnum,
+        initiator_result: CasinoBetResultTypeEnum | None = None,
         bets: list["CasinoBetAnnot"] | None = None,
     ) -> None:
         super().__init__(timeout=None)
@@ -52,6 +58,7 @@ class MultiplayerRouletteViewV2(LayoutView):
             TextDisplay[Self](
                 f"### Пользователь <@{initiator_id}> запустил игру\n"
                 f"> **Его ставка:** {initiator_bet} {coin_name_display} | {COLORS[initiator_selected_color]}\n"  # noqa: E501
+                f"> **Результат:** {initiator_result.value if initiator_result else 'Ожидание...'}\n"  # noqa: E501
             )
         )
         container.add_item(Separator[Self]())
@@ -60,20 +67,24 @@ class MultiplayerRouletteViewV2(LayoutView):
             container.add_item(
                 TextDisplay[Self](
                     "### Список остальных участников:\n"
-                    "-# пользователь - ставка - цвет"
+                    "-# пользователь - ставка - цвет - результат\n"
                     + "\n".join(
-                        f"> <@{bet['user_id']}> - {bet['bet']} - {COLORS[bet['selected_color']]}"  # noqa: E501
+                        f"> <@{bet['user_id']}> - {bet['bet']} - {COLORS[bet['selected_color']]} - {bet['result_type'].value if bet['result_type'] else 'Ожидание...'}"  # noqa: E501
                         for bet in bets
                     )
                 )
             )
 
-        container.add_item(
-            TextDisplay[Self](
-                "\nОжидается начало игры... <:sandclock:1442914884147871874>\n"
-                "Другие пользователи могут присоединиться в течение 60 секунд"
+        if state == CasinoGameStateEnum.PENDING:
+            container.add_item(
+                TextDisplay[Self](
+                    "\nОжидается начало игры... <:sandclock:1442914884147871874>\n"  # noqa: E501
+                    "Другие пользователи могут присоединиться в течение 60 секунд"  # noqa: E501
+                )
             )
-        )
+        else:
+            container.add_item(TextDisplay[Self]("\nИгра завершена.\n"))
+
         container.add_item(Separator[Self]())
 
         container.add_item(
