bdba8b96f51013344caac1226a87845ff77d2e8b
max <merzlyakov029@gmail.com>
2026-01-21 19:52:31 +0500

    refactor: simplify item change DTOs by removing heavy object dependencies

--- src/nightcore/features/economy/events/dto/item_change.py
+++ src/nightcore/features/economy/events/dto/item_change.py
@@ -4,11 +4,10 @@ from dataclasses import dataclass
 from datetime import UTC, datetime
 from typing import TYPE_CHECKING
 
-from discord import Color, Embed, Guild, Role
+from discord import Color, Embed, Guild
 
 from src.infra.db.models._annot import CaseDropAnnot
 from src.infra.db.models._enums import ItemChangeActionEnum
-from src.infra.db.models.case import Case
 
 if TYPE_CHECKING:
     from src.nightcore.bot import Nightcore
@@ -16,8 +15,8 @@ if TYPE_CHECKING:
 
 @dataclass
 class ChangedRole:
-    after: Role
-    before: Role | None = None
+    after_id: int
+    before_id: int | None = None
 
 
 @dataclass
@@ -28,8 +27,8 @@ class ChangedReward:
 
 @dataclass
 class ChangedCase:
-    after: Case
-    before: Case | None = None
+    after_name: str
+    before_name: str | None = None
 
 
 @dataclass
@@ -57,11 +56,9 @@ class ItemChangeNotifyEventDTO:
                 title = "Создание предмета"
 
             case ItemChangeActionEnum.COLOR_UPDATE.value:
-                if isinstance(self.item, ChangedRole) and self.item.before:
+                if isinstance(self.item, ChangedRole) and self.item.before_id:
                     field_name = "Изменение цвета"
-                    field_value = (
-                        f"{self.item.before.id} -> {self.item.after.id}"
-                    )
+                    field_value = f"{_format_color(self.item.before_id)} -> {_format_color(self.item.after_id)}"  # noqa: E501
 
             case ItemChangeActionEnum.UPDATE_REWARD.value:
                 field_name = "Изменение награды"
@@ -80,10 +77,13 @@ class ItemChangeNotifyEventDTO:
                     field_value = _format_reward(self.item.after)
 
             case ItemChangeActionEnum.CASE_UPDATE:
-                if isinstance(self.item, ChangedCase) and self.item.before:
+                if (
+                    isinstance(self.item, ChangedCase)
+                    and self.item.before_name
+                ):
                     field_name = "Изменение кейса"
 
-                    field_value = f"Название: {self.item.after.name} -> {self.item.before.name} "  # noqa: E501
+                    field_value = f"Название: {self.item.after_name} -> {self.item.before_name} "  # noqa: E501
             case _:
                 pass
 
@@ -115,5 +115,8 @@ def _format_reward(reward: CaseDropAnnot) -> str:
         f"{reward['name']}, "
         f"шанс: {reward['chance']}, "
         f"количество: {reward['amount']}, "
-        f"тип награды: {reward['type'].name}"
     )
+
+
+def _format_color(role_id: int):
+    return f"<@&{role_id}> ({role_id})"
