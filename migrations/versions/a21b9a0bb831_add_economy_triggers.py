78c17806fb40cd62a621b02553bc7c7091429d1b
max <merzlyakov029@gmail.com>
2026-01-23 23:22:12 +0500

    fix(migrations): change migration head

--- migrations/versions/a21b9a0bb831_add_economy_triggers.py
+++ migrations/versions/a21b9a0bb831_add_economy_triggers.py
@@ -1,7 +1,7 @@
 """add_economy_triggers
 
 Revision ID: a21b9a0bb831
-Revises: 8e6e4b99f529
+Revises: 7fedbff7e42f
 Create Date: 2026-01-23 22:28:09.801437
 
 """
@@ -14,7 +14,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = 'a21b9a0bb831'
-down_revision: Union[str, Sequence[str], None] = '8e6e4b99f529'
+down_revision: Union[str, Sequence[str], None] = '7fedbff7e42f'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
