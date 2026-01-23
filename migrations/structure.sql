64796c864db60e5e53c9abe83b11f389ab4fc7d9
max <merzlyakov029@gmail.com>
2026-01-24 00:10:42 +0500

    fix(migrations): fix trigger syntax errors and update SQL loading logic

--- migrations/structure.sql
+++ migrations/structure.sql
@@ -3,28 +3,27 @@ RETURNS TRIGGER AS $$
 BEGIN
     UPDATE battlepass_level
     SET level = level - 1
-    WHERE guild_id = OLD.guild_id AND level > OLD.level FOR UPDATE;
-    
+    WHERE guild_id = OLD.guild_id AND level > OLD.level;
     RETURN OLD;
 END;
 $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION handle_battlepass_level_insert()
 RETURNS TRIGGER AS $$
+DECLARE 
+    max_level INTEGER;
 BEGIN
     IF NEW.level IS NULL THEN
-
         SELECT COALESCE(MAX(level), 0) INTO max_level 
         FROM battlepass_level 
-        WHERE guild_id = NEW.guild_id FOR UPDATE;
-
-       NEW.level := max_level + 1
+        WHERE guild_id = NEW.guild_id 
+        FOR UPDATE;
+        NEW.level := max_level + 1;
     ELSE
        UPDATE battlepass_level
        SET level = level + 1
        WHERE guild_id = NEW.guild_id AND level >= NEW.level;
     END IF;
-
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
@@ -34,7 +33,7 @@ RETURNS TRIGGER AS $$
 BEGIN
     IF NEW.amount < 1 THEN
         DELETE FROM usercase WHERE id = NEW.id;
-
+    END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
--- migrations/versions/a21b9a0bb831_add_economy_triggers.py
+++ migrations/versions/a21b9a0bb831_add_economy_triggers.py
@@ -1,7 +1,7 @@
 """add_economy_triggers
 
 Revision ID: a21b9a0bb831
-Revises: 7fedbff7e42f
+Revises: bc10cf97b36d
 Create Date: 2026-01-23 22:28:09.801437
 
 """
@@ -14,20 +14,25 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = 'a21b9a0bb831'
-down_revision: Union[str, Sequence[str], None] = '7fedbff7e42f'
+down_revision: Union[str, Sequence[str], None] = 'bc10cf97b36d'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
     with open("migrations/structure.sql") as file:
-        query = text(file.read())
+        sql = file.read()
 
-    op.execute(query)
+    commands = sql.split("\n\n")
+
+    for command in commands:
+        op.execute(text(command))
+
 
 
 def downgrade() -> None:
     with open("migrations/drop_structure.sql") as file:
-        query = text(file.read())
+        sql = file.read()
 
-    op.execute(query)
+    for stmt in [s.strip() for s in sql.split(";") if s.strip()]:
+        op.execute(text(stmt))
